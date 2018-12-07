200430211fe76e9ad6fa7fbc55fa547b28e591f3
Naveen Bhatta <bhatta.naveen@gmail.com>
2018-12-07 17:57:32 +0530

    Ruby Syntax to Handle input & if-else block

---
+++ second.rb
@@ -0,0 +1,14 @@
+
+#puts : Similar to print() in Python
+#gets : Similar to input() in Python
+# if block is different 
+
+puts "Enter something"
+input = gets 
+
+if input.chomp.to_i >= 100 
+    puts "You are In"
+else
+    puts "You are out"    
+end
+
